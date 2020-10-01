db418c717d8cf8617d7704b799918b3ee51c30e8
Adam Winnicki <adam.baku@gmail.com>
2020-10-01 17:52:16 +0200

    feat(httpclient): add bean config of http client

---
+++ src/main/java/com/adambaku/githubapi/configuration/HttpClientConfig.java
@@ -0,0 +1,24 @@
+package com.adambaku.githubapi.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class HttpClientConfig
+{
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public HttpClient httpClient()
+    {
+        return HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(Long.parseLong(env.getProperty("http.connection.timeout", "10"))))
+            .build();
+    }
+}
