9db246f341a4b4ab4fc90b665001514ed000ea4c
Adam Winnicki <adam.baku@gmail.com>
2020-10-05 14:01:19 +0200

    feat(repo_details): implement DetailsController

--- src/main/java/com/adambaku/githubapi/web/repository/DetailsController.java
+++ src/main/java/com/adambaku/githubapi/web/repository/DetailsController.java
@@ -1,5 +1,11 @@
 package com.adambaku.githubapi.web.repository;
 
+import com.adambaku.githubapi.application.repository.query.GetDetailsQuery;
+import com.adambaku.githubapi.application.repository.query.GetDetailsQueryHandler;
+import com.adambaku.githubapi.application.repository.viewmodel.DetailsViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/repositories")
 public class DetailsController
 {
+    @Autowired
+    private ApplicationContext container;
+
     @GetMapping("/{owner}/{repository-name}")
-    public void index(@PathVariable String owner, @PathVariable("repository-name") String repositoryName)
+    public ResponseEntity<DetailsViewModel> index(@PathVariable String owner, @PathVariable("repository-name") String repositoryName)
     {
-        //@TODO
+        GetDetailsQueryHandler handler = container.getBean(GetDetailsQueryHandler.class);
+
+        DetailsViewModel viewModel = handler.handle(new GetDetailsQuery(owner, repositoryName));
+
+        return ResponseEntity.ok(viewModel);
     }
 }
