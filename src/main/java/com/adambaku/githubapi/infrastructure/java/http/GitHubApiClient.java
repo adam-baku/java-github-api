f9c6ee6a7e02b3c7083c4a3916de1ba482fe5b80
Adam Winnicki <adam.baku@gmail.com>
2020-10-05 18:37:31 +0200

    refactor(api_client): remove unused import

--- src/main/java/com/adambaku/githubapi/infrastructure/java/http/GitHubApiClient.java
+++ src/main/java/com/adambaku/githubapi/infrastructure/java/http/GitHubApiClient.java
@@ -8,7 +8,6 @@ import com.adambaku.githubapi.infrastructure.java.UrlProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
